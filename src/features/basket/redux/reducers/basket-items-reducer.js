820fa0c2bb787b6f5c7d637393f5ce08d45b2580
Jim Light <jlight@scottlogic.com>
2020-01-08 10:33:00 +0000

    basket items reducer + tests

--- src/features/basket/redux/reducers/basket-items-reducer.js
+++ src/features/basket/redux/reducers/basket-items-reducer.js
@@ -5,7 +5,6 @@ const intitialState = [];
 const reducer = (state = intitialState, action) => {
 
     if (actions.addItemToBasketAction.matches(action)) {
-
         const indexToReplace = state.findIndex(item => item.productId === action.payload.id)
 
         return indexToReplace < 0
@@ -16,6 +15,22 @@ const reducer = (state = intitialState, action) => {
             : [
                 ...state.slice(0, indexToReplace),
                 { productId: action.payload.id, quantity: state[indexToReplace].quantity + 1 },
+                ...state.slice(indexToReplace + 1)
+            ];
+    }
+
+    if (actions.removeItemFromBasketAction.matches(action)) {
+        return state.filter(x => x.productId !== action.payload);
+    }
+
+    if (actions.setItemQuantityInBasketAction.matches(action)) {
+        const indexToReplace = state.findIndex(item => item.productId === action.payload.productId)
+
+        return indexToReplace < 0
+            ? state
+            : [
+                ...state.slice(0, indexToReplace),
+                { productId: action.payload.productId, quantity: action.payload.quantity },
                 ...state.slice(indexToReplace + 1)
             ];
     }
--- src/features/basket/redux/reducers/basket-items-reducer.test.js
+++ src/features/basket/redux/reducers/basket-items-reducer.test.js
@@ -93,4 +93,117 @@ describe('basket items reducer', () => {
             ])
         })
     })
+
+    describe(actions.removeItemFromBasketAction.type, () => {
+        it('should do nothing if basket is undefined', () => {
+            const state = undefined;
+            expect(
+                reducer(state, actions.removeItemFromBasketAction.create(1))
+            ).toEqual([])
+        });
+
+        it('should do nothing if basket is empty', () => {
+            const state = [];
+            expect(
+                reducer(state, actions.removeItemFromBasketAction.create(1))
+            ).toEqual([])
+        });
+
+        it('should do nothing if basket is not empty but id on action not in basket', () => {
+            const state = [
+                { productId: 1 }
+            ];
+            expect(
+                reducer(state, actions.removeItemFromBasketAction.create(2))
+            ).toEqual([
+                { productId: 1 }
+            ]);
+        });
+
+        it('should remove item from basket if productId is in basket', () => {
+            const state = [
+                { productId: 1 },
+                { productId: 2 },
+                { productId: 3 }
+            ];
+            expect(
+                reducer(state, actions.removeItemFromBasketAction.create(2))
+            ).toEqual([
+                { productId: 1 },
+                { productId: 3 }
+            ]);
+        });
+    })
+
+    describe(actions.setItemQuantityInBasketAction.type, () => {
+        it('should do nothing if basket is undefined', () => {
+            const state = undefined;
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 1 }))
+            ).toEqual([])
+        });
+
+        it('should do nothing if basket is empty', () => {
+            const state = [];
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 1 }))
+            ).toEqual([])
+        });
+
+        it('should do nothing if basket is not empty but id on action not in basket', () => {
+            const state = [
+                { productId: 1 }
+            ];
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 2, quantity: 1 }))
+            ).toEqual([
+                { productId: 1 }
+            ]);
+        });
+
+        it('should change quantity of item in basket when productId matches and item is first of 3', () => {
+            const state = [
+                { productId: 1, quantity: 1 },
+                { productId: 2, quantity: 1 },
+                { productId: 3, quantity: 1 }
+            ];
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 1, quantity: 2 }))
+            ).toEqual([
+                { productId: 1, quantity: 2 },
+                { productId: 2, quantity: 1 },
+                { productId: 3, quantity: 1 }
+            ])
+        });
+
+        it('should change quantity of item in basket when productId matches and item is second of 3', () => {
+            const state = [
+                { productId: 1, quantity: 1 },
+                { productId: 2, quantity: 1 },
+                { productId: 3, quantity: 1 }
+            ];
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 2, quantity: 2 }))
+            ).toEqual([
+                { productId: 1, quantity: 1 },
+                { productId: 2, quantity: 2 },
+                { productId: 3, quantity: 1 }
+            ])
+        });
+
+        it('should change quantity of item in basket when productId matches and item is third of 3', () => {
+            const state = [
+                { productId: 1, quantity: 1 },
+                { productId: 2, quantity: 1 },
+                { productId: 3, quantity: 1 }
+            ];
+            expect(
+                reducer(state, actions.setItemQuantityInBasketAction.create({ productId: 3, quantity: 2 }))
+            ).toEqual([
+                { productId: 1, quantity: 1 },
+                { productId: 2, quantity: 1 },
+                { productId: 3, quantity: 2 }
+            ])
+        });
+    })
 })
